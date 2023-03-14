0ab39a9294bc5722a1178e4fcca5d8cfaab0736e
Jeremy Field <jdf@jdf.id.au>
2023-03-14 22:16:37 +1100

    gratuitous refactoring

--- src/user.clj
+++ src/user.clj
@@ -1,94 +1,110 @@
 (ns user
   (:require [clojure.java.io :as io]
             [clojure.data.xml :as xml]
-            [clojure.walk :as walk]))
+            [clojure.walk :as walk]
+            [clojure.test :refer [with-test is]]))
 
-(def mbs
-  ;; Could stream it but only 8MB and less pleasant to dev with.
-  ;; Could spec it but not for proof of concept.
-  ;; Gives approximately:
-  #_{:tag :MBS_XML
-     :content ({:tag :Data
-                :content ({:tag :ItemNum
-                           :content ("3")}
-                          {:tag :Description
-                           :content ("string\n")})})}
-  (-> "MBS-XML-20230301-v2.XML" io/resource slurp xml/parse-str))
+(def mbs "MBS-XML-20230301-v2.XML")
 
-(def item-numbers
-  (do
-    (assert (and (= (:tag mbs) :MBS_XML) (empty? (:attrs mbs))))
-    (into (sorted-map)
-      (for [{:keys [tag attrs content]} (:content mbs)
-            :let [_ (assert (and (= tag :Data) (empty? attrs)))
-                  {item-num :ItemNum :as item-data}
-                  (into {}
-                    (for [{:keys [tag attrs content] :as data-tag} content
-                          :let [;_ (println data-tag)
-                                _ (assert (and (empty? attrs)
-                                            (<= (count content) 1)))]]
-                      [tag (first content)]))]]
-        [(Integer/parseInt item-num) item-data]))))
+(def parse (comp xml/parse-str slurp io/resource))
+
+#_(time (and (parse mbs) nil))
+;; Could stream it but only 8MB and less pleasant to dev with.
+;; Could spec it but not for proof of concept.
+;; Gives approximately:
+#_{:tag :MBS_XML
+   :content ({:tag :Data
+              :content ({:tag :ItemNum
+                         :content ("3")}
+                        {:tag :Description
+                         :content ("string\n")})})}
+
+(defn item-numbers
+  [mbs]
+  (assert (and (= (:tag mbs) :MBS_XML) (empty? (:attrs mbs))))
+  (into (sorted-map)
+    (for [{:keys [tag attrs content]} (:content mbs)
+          :let [_ (assert (and (= tag :Data) (empty? attrs)))
+                {item-num :ItemNum :as item-data}
+                (into {}
+                  (for [{:keys [tag attrs content] :as data-tag} content
+                        :let [;_ (println data-tag)
+                              _ (assert (and (empty? attrs)
+                                          (<= (count content) 1)))]]
+                    [tag (first content)]))]]
+      [(Integer/parseInt item-num) item-data])))
 
 (def time-descriptions
-  (->> item-numbers
+  (comp
     (filter (fn [[k v]] (< 23000 k 25000)))
     (map (fn [[k {:keys [Description]}]] [k Description]))))
 
-(defn parse-desc
-  "Return upper time threshold in minutes. Case-insensitive."
-  [s]
-  (let [[_ _ just-min h m]
-        (re-find #"(?i).* to ((\d\d) minutes|(\d?\d):(\d\d) hours?)" s)]
-    (cond
-      just-min (Integer/parseInt just-min)
-      (and h m) (+ (* (Integer/parseInt h) 60) (Integer/parseInt m)))))
+(with-test
+  (defn parse-desc
+    "Return upper time threshold in minutes. Case-insensitive."
+    [s]
+    (let [[_ _ just-min h m]
+          (re-find #"(?i).* to ((\d\d) minutes|(\d?\d):(\d\d) hours?)" s)]
+      (cond
+        just-min (Integer/parseInt just-min)
+        (and h m) (+ (* (Integer/parseInt h) 60) (Integer/parseInt m)))))
+  (is (= (parse-desc "31 MINUTES to 45 MINUTES (3 basic units)\n") 45))
+  (is (= (parse-desc "17:41 HOURS TO 17:50 HOURS (103 basic units)\n") 1070)))
 
-#_(parse-desc "31 MINUTES to 45 MINUTES (3 basic units)\n")
-#_(parse-desc "17:41 HOURS TO 17:50 HOURS (103 basic units)\n")
-
-(def time-cutoffs
+(defn time-cutoffs-impl
   "In minutes"
+   [time-descriptions]
   (into (sorted-map 15 23010) ; NB hardcode special case
     (for [[k v] (rest time-descriptions)]
       [(parse-desc v) k])))
 
-(defn int24h->min [i]
-  (let [h (quot i 100)
-        m (rem i 100)]
-    (assert (<= m 60))
-    (+ (* h 60) m)))
+(def time-cutoffs
+  (memoize (fn [f] (->> f parse item-numbers (eduction time-descriptions) time-cutoffs-impl))))
 
-(defn duration
-  "Case duration in minutes.
-  Times as integer statement of 24h clock. Can span midnight."
-  ;; Separate function could detect "unsocial hours" but would need date.
-  [from to]
-  (let [f (int24h->min from)
-        t (int24h->min to)]
-    (if (< f t) (- t f) (- (+ t (* 24 60)) f))))
+#_(->> "MBS-XML-20230301-v2.XML" time-cutoffs time)
 
-(defn min->str [d]
-  (let [h (quot d 60)
-        m (rem d 60)]
-    (format "%d:%02d" h m)))
+(with-test
+  (defn str24h->min [s]
+    (let [x (Integer/parseInt s 10)
+          h (quot x 100)
+          m (rem x 100)]
+      (assert (< m 60))
+      (+ (* h 60) m)))
+  (is (= (str24h->min "0101") 61)))
 
-#_(min->str 1439)
+(with-test
+  (defn duration
+    "Case duration in minutes.
+    Times as string \"0000-2222\". Can span midnight."
+    ;; Separate function could detect "unsocial hours" but would need date.
+    [s]
+    (let [[_ f t] (re-find #"(\d{4})-(\d{4})" s)
+          [f t] (map str24h->min [f t])]
+      (if (< f t) (- t f) (- (+ t (* 24 60)) f))))
+  (is (= (duration "0000-1023") 623))
+  (is (= (duration "2304-0200") 176)))
+
+(with-test
+  (defn min->str [d]
+    (let [h (quot d 60)
+          m (rem d 60)]
+      (format "%d:%02d" h m)))
+  (is (= (min->str 1439) "23:59")))
 
 (defn item [duration]
   {:pre [(<= duration (* 24 60))]}  
-  (->> time-cutoffs
+  (->> mbs time-cutoffs
     (drop-while (fn [[cutoff item]] (< cutoff duration)))
     first
     val))
 
-(defn duration+item
-  [from to]
-  (let [d (duration from to)
-        i (item d)
-        s (min->str d)]
-    [s i]))
+(with-test
+  (defn duration+item
+    [s]
+    (let [d (duration s)
+          i (item d)
+          s (min->str d)]
+      [s i]))
+  (is (= (duration+item "1100-2330") ["12:30" 23670])))
 
 (def d+i duration+item)
-
-#_ (d+i 1100 2330)
